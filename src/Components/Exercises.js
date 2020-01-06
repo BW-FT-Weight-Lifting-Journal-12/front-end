17f0d04c4b2642e959825a05d6dea76a18ffc922
Robert Stepanov <robertdstepanov@gmail.com>
2020-01-06 12:58:50 -0500

    Initial Setup

---
+++ src/Components/Exercises.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Exercises = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Exercises Page</h1>
+    </div>
+  );
+};
+
+export default Exercises;
---
+++ src/Components/MyExercises.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const MyExercises = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>My Exercises</h1>
+    </div>
+  );
+};
+
+export default MyExercises;
