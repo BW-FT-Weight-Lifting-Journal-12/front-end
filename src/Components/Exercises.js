df7042b0d4913a67d89b544943e41cacce5fd2e6
Robert Stepanov <robertdstepanov@gmail.com>
2020-01-06 17:25:02 -0500

    Add Exercise Form added and List setup

--- src/Components/Exercises.js
+++ src/Components/Exercises.js
@@ -1,11 +1,39 @@
 import React from "react";
 import Navigation from "./Navigation";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 const Exercises = () => {
   return (
     <div>
       <Navigation />
-      <h1>Exercises Page</h1>
+      <h1>Add Exercise to My Workout</h1>
+
+      <Form className="form-wrapper">
+        <FormGroup className="form-grp">
+          <Label for="exampleSelect">Exercise: </Label>
+          <Input type="select" name="select" id="exercise">
+            <option>Select One</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Input>
+        </FormGroup>
+        <FormGroup className="form-grp">
+          <Label for="exampleEmail">Weight: </Label>
+          <Input type="text" name="weight" id="weight" />
+        </FormGroup>
+        <FormGroup className="form-grp">
+          <Label for="sets">Sets: </Label>
+          <Input type="text" name="sets" id="sets" />
+        </FormGroup>
+        <FormGroup className="form-grp">
+          <Label for="reps">Reps: </Label>
+          <Input type="text" name="reps" id="reps" />
+        </FormGroup>
+
+        <Button className="exercise-btn">Add Exercise</Button>
+      </Form>
     </div>
   );
 };
--- src/Components/MyExercises.js
+++ src/Components/MyExercises.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Navigation from "./Navigation";
+import { Card, CardTitle, CardSubtitle, CardBody, CardDeck } from "reactstrap";
 
 const MyExercises = () => {
+
+  const workouts = [
+    { id: 1, name: "Benchpress", sets: "", reps: "", weight: "" },
+    { id: 2, name: "Bicep curls", sets: "", reps: "", weight: "" },
+    { id: 3, name: "Leg Press", sets: "", reps: "", weight: "" },
+    { id: 4, name: "Lat Pulldowns", sets: "", reps: "", weight: "" }
+  ];
+
   return (
     <div>
       <Navigation />
       <h1>My Exercises</h1>
+      {workouts.map(exercise => {
+        return (
+          <CardDeck className="wrapper">
+          <Card className="card-wrapper" key={workouts.id}>
+          <CardBody className="card-body">
+            <CardTitle>Name: {exercise.name}</CardTitle>
+            <CardSubtitle>Sets: {exercise.sets}</CardSubtitle>
+            <CardSubtitle>Reps: {exercise.reps}</CardSubtitle>
+            <CardSubtitle>Weight: {exercise.weight}</CardSubtitle>
+          </CardBody>
+          </Card>
+          </CardDeck>
+        );
+      })}
     </div>
   );
 };
