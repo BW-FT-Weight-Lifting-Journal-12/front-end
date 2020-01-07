1f680a0d5eec817f377b0ad770956bebdbfb8a07
wais <waisalmakaleh@gmail.com>
2020-01-06 22:04:37 -0500

    added prive route

---
+++ src/Components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+
+const PrivateRoute = ( { component: Component, ...rest } ) =>
+{
+    return (
+        <Route
+            { ...rest }
+            render={ props =>
+            {
+                if ( localStorage.getItem( 'token' ) )
+                {
+                    // render component from props
+                    return <Component { ...props } />;
+                } else
+                {
+                    return <Redirect to="/login" />;
+                }
+            } }
+        />
+    );
+};
+
+export default PrivateRoute;
